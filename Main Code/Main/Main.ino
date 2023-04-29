f881d2bb3e8d798d6db9334782619bb7c0490177
Emmagx <emmagx.dev@gmail.com>
2023-04-29 15:19:22 -0600

    Update Main.ino

--- Main Code/Main/Main.ino
+++ Main Code/Main/Main.ino
@@ -1,41 +1,36 @@
-//Definir Librerias a Usar
-#include <WiFi.h>
-#include <ThingSpeak.h>
-#include <MQUnifiedsensor.h>
-//#include <WiFiManager.h>
-#include "DHT.h"
-#include "WiFi.h"
+//Este codigo esta separado por Funciones, y todas son llamadas en el setup o en el LOOP
 
-#include "arduino_secrets.h"
+//Definimos las librerias
+#include <ThingSpeak.h> //Libreria Oficial de ThingsPeak que nos permite comunicarnos con la NUBE
+#include <MQUnifiedsensor.h> //Libreria de todos los Sensores MQ
+#include <WiFiManager.h> //Libreria que nos permite abrir un servidor web para conectar a conexiones wifi.
+#include "DHT.h" //Libreria para usar el modulo DHT
 
-const char* ssid = SECRET_SSID;
-
-const char* password = SECRET_PSW;
 
 //Declaramos las credenciales wifi por Defecto
-//const char* ssid="BITMEC_PRODUCCION";
-//const char* password="Produccion22*";
+const char* ssid = ""; // El SSID es el nombre de tu RED WIFI, o sea, depende de tu casa. Esto se define en la pestana arduino_secrets.h 
+const char* password = ""; // Contraseña de tu WIFI o del WIfi al que lo quieras conectar por defecto. Esto se define en la pestana arduino_secrets.h 
 
 //Definimos los Pin y e tipo de sensor DHT: DHT11 o DHT22
-#define DHTPIN 14
-#define DHTTYPE DHT11
-//Se define el sensor DHT
-DHT dht(DHTPIN, DHTTYPE);
+#define DHTPIN 32 //Solo se pueden usar estos pin 32, 33, 34, 35, 36, 37, 38 y 39. Ya que al conectar el ESP32 a Wifi no permite leer otros pin de manera analogica 
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);//Creamos un objeto con las propiedades dadas anteriormente.
 
 //Definimos las credenciales para la conexión a la plataforma
-//Agregamos el ID del Canal al que nos queremos comunicar
 unsigned long channelID = 2118820; //Numero/Nombre del canal
-//Definimos la Api Key que vamos a usar.
-const char* WriteAPIKey="4WPMV3E0BN7F1B2P"; 
+const char* WriteAPIKey="4WPMV3E0BN7F1B2P"; //Definimos la Api Key que vamos a usar.
 
 //Se definen todos valores de los sensores MQ.
+//En los pines, solo se pueden utilizar estos 32, 33, 34, 35, 36, 37, 38 y 39. Ya que al conectar el ESP32 a Wifi no permite leer otros pin de manera analogica
+//!!!!!!!!!!!IMPORTANTE: Un pin por Sensor!!!!!!!!!!!!!1
+
 //Definicion    Nombre Variable         Valor
 #define         Board                   ("ESP32") //Definimos la tarjeta en una variable a usar: ESP32, ESP8622, ARDUINO 1, ARDUINO Nano...
-#define         MQ7Pin                  (4)  //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-7 
+#define         MQ7Pin                  (33)  //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-7 
 #define         MQ7Type                 ("MQ-7") //Definimos el sensor quee estara en ese Pin Definido.
-#define         MQ5Pin                  (25) //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-5 
+#define         MQ5Pin                  (34) //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-5 
 #define         MQ5Type                 ("MQ-5") //Definimos el sensor quee estara en ese Pin Definido.
-#define         MQ4Pin                  (21) //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-4
+#define         MQ4Pin                  (35) //Analog input, definimos en una variable el Pin que vamos a usar en el Sensor MQ-4
 #define         MQ4Type                 ("MQ-4") //Definimos el sensor quee estara en ese Pin Definido.
 #define         Voltage_Resolution      (5) //Voltaje que usaremos: 5v, 3.3v
 #define         ADC_Bit_Resolution      (12) //Cantidad de bits de informacion que tolera nuestra tarjeta. Ver documentacion individual de la tarjeta 
@@ -47,49 +42,37 @@ const char* WriteAPIKey="4WPMV3E0BN7F1B2P";
 MQUnifiedsensor MQ7(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ7Pin, MQ7Type); //sensor MQ-7
 MQUnifiedsensor MQ5(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ5Pin, MQ5Type); //sensor MQ-5
 MQUnifiedsensor MQ4(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ4Pin, MQ4Type); //sensor MQ-4
+
 //DECLARAMOS las variables que usaremos en tipo flotante(Decimal), sin valor definido
 float dioxido; //Variable que almacenera el dato del Dioxido
 float humedad; //Variable que almacenera el dato del Humedad
 float metano;  //Variable que almacenera el dato del Metano
 float propano; //Variable que almacenera el dato del Propano
 float temperatura; //Variable que almacenera el dato del Temperatura
+
 //Variables para la funcion MILIS
 unsigned long lastTime = 0; // variable para almacenar el tiempo de la última lectura
 const unsigned long interval = 5000; // intervalo de tiempo entre lecturas (en milisegundos)
+
 //Se inicializa el servicio de WiFi en modo cliente.
 WiFiClient cliente;
-//Codigo que se almacenara lo que se hara en la inicializacion de la tarjeta.
-void setup() {
-   Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
+//Funcion que nos conecta a Thinkspeak.
+void connectToThingSpeak() {
+  ThingSpeak.begin(cliente); //Iniciamos el servicio Thinkspeak con el WIFI cliente que definimos anteriormente
+  delay(5000); // Espera 5 segundos para establecer la conexion.
+}
 
-  while (WiFi.status() != WL_CONNECTED) {
-
-    delay(500);
-
-    Serial.println("Connecting to WiFi..");}
-  //WiFi.mode(WIFI_STA);  //Colocamos el modo Wifi a modo estacion
-  Serial.begin(115200); //Colocamos en que frecuencia se va a leer el monitor serial
-
-  //WiFiManager wiFiManager; //Revisa las credenciales de WIFI, 
-  //wiFiManager.resetSettings(); //resetea los datos de las redes wifi antes conocidas
-  //bool res = wiFiManager.autoConnect("Dispositivo", "12345678"); //inicia un servidor web para poder conectarnos a la placa y asi manejar las credenciales de WiFi
-  //prueba la conexion a internet
-  //if (!res) {
-    //Serial.println("Failed to connect!");// sino se pudo conectar
-  //} else {
-    //Serial.println("Connected :)");     //Si se encuentra conectado
-  //}
-//Conectamos a thinkspeak
-  ThingSpeak.begin(cliente);
-  delay(5000); //Esperamos 5 segundos para darle tiempo para conectarse
-//inicializamos el sensor DHT
-  dht.begin();
-
-  //CO Parametros
-  MQ7.setRegressionMethod(1); //Partes por Millon, como quiero que se presenten los datos.
+//Funcion que inicializa todos los sensores
+void initializeSensors() {
+  dht.begin(); //Inicializa DHT
+  MQ7.init();  //Inicializa MQ7
+  MQ5.init();  //Inicializa MQ5
+  MQ7.init();  //Inicializa MQ4
+  
+  MQ7.setRegressionMethod(1); // Forma de como vamos a leer los datos 1= Forma exponencial. PPM
   /*
+  Exponential regression:
   GAS     | a      | b
   H2      | 69.014  | -1.374
   LPG     | 700000000 | -7.703
@@ -97,11 +80,10 @@ void setup() {
   CO      | 99.042 | -1.518
   Alcohol | 40000000000000000 | -12.35
   */
-  MQ7.setA(99.042); MQ7.setB(-1.518); // Valor depende del GAS a leer
-  MQ7.setR0(0.2868324816); // Valor encontrado al analizar el sensor con el codigo de calibración.
+  MQ7.setA(99.042);  MQ7.setB(-1.518);  //Datos brindados por el Datasheet, depende del gas a leer por cada sensor.
+  MQ7.setR0(0.2868324816); //Valor que encontramos al calibrar los sensores. 
 
-  //C3H8 Parametros
-  MQ5.setRegressionMethod(1); //Partes por Millon, como quiero que se presenten los datos.
+  MQ5.setRegressionMethod(1); // Forma de como vamos a leer los datos 1= Forma exponencial. PPM
   /*
   Exponential regression:
   Gas    | a      | b
@@ -111,76 +93,112 @@ void setup() {
   CO     | 491204 | -5.826
   Alcohol| 97124  | -4.918
   */
-  MQ5.setA(80.897); MQ5.setB( -2.4312); // A Y B depende del gas a Leer.
-  MQ5.setR0(3.0120439529); //Valor encontrado al analizar el sensor con el codigo de calibración.
-
-  //LDP Parametros
-  MQ4.setRegressionMethod(1); //Partes por Millon, como quiero que se presenten los datos.
-    /*
-    Exponential regression:
+  MQ5.setA(80.897);  MQ5.setB(-2.4312); //Datos brindados por el Datasheet, depende del gas a leer por cada sensor.
+  MQ5.setR0(3.0120439529); //Valor que encontramos al calibrar los sensores. 
+  
+  MQ4.setRegressionMethod(1); // Forma de como vamos a leer los datos 1= Forma exponencial. PPM
+  /*
+  Exponential regression:
   Gas    | a      | b
-  H2     | 1163.8 | -3.874
-  LPG    | 80.897 | -2.431
-  CH4    | 177.65 | -2.56
-  CO     | 491204 | -5.826
-  Alcohol| 97124  | -4.918
+  LPG    | 3811.9 | -3.113
+  CH4    | 1012.7 | -2.786
+  CO     | 200000000000000 | -19.05
+  Alcohol| 60000000000 | -14.01
+  smoke  | 30000000 | -8.308
   */
-  MQ4.setA(1012.7); MQ4.setB(-2.786); //// A Y B depende del gas a Leer.
-  MQ4.setR0(1.7213886976);
-  // Con el Metodo de Regresion 1:
-  //*********Esta es la formula que utiliza la libreria para dar los datos: PPM =  a*ratio^b *****************
+  MQ4.setA(1012.7);  MQ4.setB(-2.786);  //Datos brindados por el Datasheet, depende del gas a leer por cada sensor.
+  MQ4.setR0(1.7213886976); //Valor que encontramos al calibrar los sensores. 
 }
-//Aqui empieza el codigo Principal.
-void loop() {
-  //Iniciamos la Lectura del Sensor DHT, con una funcion Milis, que dejara seguir usando los demas sensores y cada 5 segundos lea la temperatura.
-  unsigned long currentTime = millis(); //Le damos este valor a la variable que nos va a ayudar a mantener la funcion
-    if (currentTime - lastTime >= interval) { //empezamos la funcion con un parametro que cuando iguale al intervalo va a realizarse.
-    lastTime = currentTime; //iguala ambas variables
 
-    humedad = dht.readHumidity(); //Leemos humedad con una funcion ya definida por la libreria
-    temperatura = dht.readTemperature(); //Leemos Temperatura con una funcion ya definida por la libreria
-    //Imprimimos valores en el monitor serial, para poder verlos en caso de no tener conexion a nuestra nube IOT
-    Serial.print("temperatura: "); // Imprimimos temperatura y sus datos.
+void connectToWiFi() {
+  Serial.println("Connecting to WiFi..");
+  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);
+
+  WiFiManager wiFiManager;
+  //wiFiManager.resetSettings();
+  bool res = wiFiManager.autoConnect("Dispositivo", "12345678"); //este es el nombre y la contraseña que se le da a donde nos conectaremos para darle una red wifi
+  //prueba la conexion a internet
+  if (!res) {
+    Serial.println("Failed to connect!");
+  } else {
+    Serial.println("Connected :)");
+  }
+}
+
+void setup() {
+  Serial.begin(115200); //Iniciamos el monitor serial a 115200 
+  //Llamamos a las funciones que definimos anteriormente
+  connectToWiFi(); 
+  connectToThingSpeak();
+  initializeSensors();
+}
+/*Funciones que se usaran en el LOOP*/
+
+//Funcion que lee el sensor DHT
+void leerSensorDHT() {
+  //Leemos cada 5s el sensor DHT sin interrumpir la lectura de los demas sensores
+  unsigned long currentTime = millis(); //Iguala los tiempos
+  if (currentTime - lastTime >= interval) { //Entra en un bucle si el ultimo tiempo y el ultimo tiempo registrado siguen siendo mayores al intervalo
+    lastTime = currentTime;
+    humedad = dht.readHumidity(); //Lee el sensor DHT y le da su valor a la variable humedad
+    temperatura = dht.readTemperature(); //lee el sensor DHT y le da su valor a la variable temperatura.
+    /*Imprimimos los datos en el monitor Serial*/
+    Serial.print("temperatura: "); 
     Serial.print(temperatura);
     Serial.println(" °C");
-    Serial.print("Humedad: "); //Imprimimos Humedad y sus datos.
+    Serial.print("Humedad: ");
     Serial.print(humedad);
-    Serial.print("%");}
+    Serial.print("%");
+  }
+}
 
-  MQ7.init(); //Inicializamos sensor MQ7
-  MQ7.update(); //Llama funciones de la libreria
-  MQ7.readSensor(); //Lee el sensor con la formula dada anteriormente
-  dioxido = MQ7.readSensor(); //Le da el valor a la variable dioxido el de la lectura
-  Serial.print("CO: "); // Imprime datos de lecturas
-  Serial.println(dioxido,10); // Imprime el valor con 10 decimales
-  delay(1000); //Damos una separación de un segundo con el Otro sensor
+//Funcion que lee el sensor MQ7
+void leerSensorMQ7() {
+  MQ7.update(); //Llamamos la funcion update
+  dioxido = MQ7.readSensor(); //le damos el valor que leímos a la variable dioxido
+  //Imprimimos los valores en el monitor Serial
+  Serial.print("CO: ");
+  Serial.println(dioxido,10); 
+  delay(1000); //damos un segundo de delay para la siguiente medición
+}
 
-  MQ5.init(); //Inicializamos Sensor MQ5
-  MQ5.update(); //Llama funciones de la Libreria.
-  MQ5.readSensor(); //Llama a la funcion de leer sensor
-  propano = MQ5.readSensor(); //Le da el valor a la variable propano de la lectura
-  Serial.print("GAS: "); //Imprime los datos En el monitor Serial
-  Serial.println(propano, 10); //Imprime los valores con 10 decimales.
-  delay(1000); //Damos una separación de un segundo con el Otro sensor
+//Funcion que lee el sensor MQ5
+void leerSensorMQ5() {
+  MQ5.update(); //Llamamos la funcion update
+  propano = MQ5.readSensor(); //le damos el valor que leímos a la variable propano
+  //Imprimimos los valores en el monitor Serial
+  Serial.print("GAS: ");
+  Serial.println(propano, 10);
+  delay(1000);//damos un segundo de delay para la siguiente medición
+}
 
-  MQ4.init(); //Incializamos sensor MQ4
-  MQ4.update(); //Llamamos a todas las funciones
-  MQ4.readSensor(); //Llamamos La funcion que lee el sensor
-  metano = MQ4.readSensor(); //Le damos a la variable Metano el valor leido
-  Serial.print("LDP: "); //Imprimimos en el monitor serial Los valores encontrados
-  Serial.println(metano,20); //Imprimimos el valor del metano con 20 decimales.
-  delay(1000); //Agregamos una pausa de un segundo para la siguiente funcion.
+//Funcion que lee el sensor MQ5
+void leerSensorMQ4() {
+  MQ4.update(); //Llamamos la funcion update
+  metano = MQ4.readSensor(); //le damos el valor que leímos a la variable metano
+  //Imprimimos los valores en el monitor Serial
+  Serial.print("LDP: ");
+  Serial.println(metano,20); 
+  delay(1000);//damos un segundo de delay para la siguiente medición
+}
+//Función que envia los datos a la NUBE
+void enviarDatosThingSpeak() {
+  ThingSpeak.writeFields(channelID,WriteAPIKey); //Nos subscribimos a la nube con el ID y el API ya definimos arriba
+  Serial.println("Datos enviados a ThingSpeak!"); 
+  delay(10);
+  ThingSpeak.setField(1,temperatura); //Llenamos el campo 1 en thingspeak con el valor temperatura, el numero depende del campo a llenar y lo acompaña la variable con el dato a llenar.
+  ThingSpeak.setField(2,humedad);     //Llenamos el campo 2 en thingspeak con el valor humedad, el numero depende del campo a llenar y lo acompaña la variable con el dato a llenar.
+  ThingSpeak.setField(3,metano);      //Llenamos el campo 3 en thingspeak con el valor metano, el numero depende del campo a llenar y lo acompaña la variable con el dato a llenar.
+  ThingSpeak.setField(4,propano);     //Llenamos el campo 4 en thingspeak con el valor propano, el numero depende del campo a llenar y lo acompaña la variable con el dato a llenar.
+  ThingSpeak.setField(5,dioxido);     //Llenamos el campo 5 en thingspeak con el valor dioxido, el numero depende del campo a llenar y lo acompaña la variable con el dato a llenar.
+}
 
-//Hacemos la conexión y envío de datos a la plataforma, utilizando las credenciales definidas anteriormente
-  ThingSpeak.writeFields(channelID,WriteAPIKey);
-//Imprimimos una frase indicando el envío, y agregamos un retardo de 10 segundos*/
-  Serial.println("Datos enviados a ThingSpeak!");
-  delay(10000);  
-//Indicamos el orden de envío por campos o Field, en el orden definido de la plataforma, junto a los valores del sensor
-//Función             Campo, Nombre de la variable que rellenará el campo
-  ThingSpeak.setField(1,temperatura); //como leer esta parte del codigo: "Mando al campo 1 de Things speak el valor de la variable Temperatura"
-  ThingSpeak.setField(2,humedad); //como leer esta parte del codigo: "Mando al campo 2 de Things speak el valor de la variable Humedad"
-  ThingSpeak.setField(3,metano);  //como leer esta parte del codigo: "Mando al campo 3 de Things speak el valor de la variable Metano"
-  ThingSpeak.setField(4,propano); //como leer esta parte del codigo: "Mando al campo 4 de Things speak el valor de la variable Propano"
-  ThingSpeak.setField(5,dioxido); //como leer esta parte del codigo: "Mando al campo 5 de Things speak el valor de la variable Dioxido"
+//Llamamos a las funciones ya definidas anteriormente.
+void loop() {
+  leerSensorDHT();
+  leerSensorMQ7();
+  leerSensorMQ5();
+  leerSensorMQ4();
+  enviarDatosThingSpeak();
 }
